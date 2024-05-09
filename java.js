cf22a6d5d5b3dc131055e67f601a184756e047ef
Derick <finederrick7@gmail.com>
2024-05-09 16:46:38 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,10 @@
+function openwindow(){
+    var myName = document.getElementById("username")
+    var myPassword = document.getElementById("password")
+    if(myName.value === "admin" && myPassword.value == "1234"){
+        alert("Welcome");
+    } 
+    else{
+               alert("Retry");
+    }
+ }
